6d4e3ace8a6a9e8850d0ebebe0b7031e1e223665
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-22 16:10:37 -0300

    :left_right_arrow: Connect Mysql with NodeJS

---
+++ saboroso/inc/db.js
@@ -0,0 +1,21 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'restaurant',
+    password: 'Cle@r2022'
+});
+
+module.exports = connection;
+
+// // simple query
+// connection.query(
+//     'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//     function(err, results, fields) {
+//         console.log(results); // results contains rows returned by server
+//         console.log(fields); // fields contains extra meta data about results, if available
+//     }
+// );
