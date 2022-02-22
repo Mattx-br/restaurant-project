8b94986c93f62003ad92b2b40b2e6767aa468c37
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-22 16:42:17 -0300

    :twisted_rightwards_arrows: Make menu on index page acts dynamically

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,9 +1,26 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    conn.query('SELECT * FROM tb_menus ORDER BY title', (err, result) => {
+
+        if (err) {
+
+            console.log(err);
+            res.send(err);
+
+        }
+
+        res.render('index', {
+            title: 'Restaurante Saboroso!',
+            menus: result
+        });
+
+    })
+
 });
 
 module.exports = router;
