d90df7c1eb237b7f93b2e9f62236e9a74eaf3f08
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-25 15:45:16 -0300

    Restarting from redis

--- inc/users.js
+++ inc/users.js
@@ -44,7 +44,7 @@ module.exports = {
 
                             } else {
 
-                                resolve(result);
+                                resolve(row);
 
                             }
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,117 +1,79 @@
 var express = require('express');
-var users = require('./../inc/users');
 var router = express.Router();
-var admin = require('../inc/admin');
-var menus = require('../inc/menus');
 
-// middleware
-router.use(function(req, res, next) {
+var users = require('./../inc/users')
 
-    if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
+router.get('/', function (req, res, next) {
 
-        res.redirect('/admin/login');
-
-    } else {
-
-        next();
-
-    }
-    // res.render('admin/index');
-
-    console.log('\nMiddleWare', req.url + '\n\n');
+    res.render('admin/index', {
 
 
-});
 
-router.use(function(req, res, next) {
-
-    req.menus = admin.getMenus(req);
-
-    next();
+    });
 
 });
 
 
-// get Methods
-
-router.get('/logout', function(req, res, next) {
-
-    // console.log('user', req.session.user + '\n\n');
-
-    delete req.session.user;
-
-    console.log('user dps de deletar', req.session.user + '\n\n');
-
-
-    // console.log('\nMiddleWare', req.url + '\n\n');
-
-    res.redirect('/admin/login');
-
-})
-
-router.get('/', function(req, res, next) {
-
-
-    admin.dashboard().then(data => {
-
-        res.render('admin/index', admin.getParams(req, {
-            data
-        }));
-
-    }).catch(err => { console.log(err) });
-
-});
-
-
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
 
     users.render(req, res, null);
 
 });
 
-router.get('/contacts', function(req, res, next) {
+router.get('/contacts', function (req, res, next) {
 
-    res.render('admin/contacts', admin.getParams(req));
+    res.render('admin/contacts', {
 
-});
 
-router.get('/menus', function(req, res, next) {
-
-    menus.getMenus().then(data => {
-
-        res.render('admin/menus', admin.getParams(req, {
-            data
-        }));
 
     });
 
 });
 
-router.get('/reservations', function(req, res, next) {
+router.get('/menus', function (req, res, next) {
 
-    res.render('admin/reservations', admin.getParams(req, { date: {} }));
+    res.render('admin/menus', {
 
-});
 
-router.get('/users', function(req, res, next) {
 
-    res.render('admin/users', admin.getParams(req));
+    });
 
 });
 
-router.get('/emails', function(req, res, next) {
+router.get('/reservations', function (req, res, next) {
+
+    res.render('admin/reservations', {
+
+        date: {}
+
+    });
+
+});
+
+router.get('/users', function (req, res, next) {
+
+    res.render('admin/users', {
+
+
+
+    });
+
+});
+
+router.get('/emails', function (req, res, next) {
 
     res.render('admin/emails', {
 
-        menus: req.menus,
-        user: req.session.user
+
 
     });
 
 });
 
-
+// ======================================
 // post Methods
+// ======================================
+
 router.post('/login', function(req, res, next) {
 
     if (!req.body.email) {
@@ -126,7 +88,7 @@ router.post('/login', function(req, res, next) {
 
     } else {
 
-        console.log('\nentro no else\n');
+        console.log('\nAcertou email e senha ou..\n');
 
         users.login(req.body.email, req.body.password)
             .then(user => {
@@ -139,24 +101,13 @@ router.post('/login', function(req, res, next) {
             .catch(err => {
 
                 console.log('login errado\n');
-                users.render(req, res, err.message)
+                users.render(req, res, 'Email or password invalid');
 
             });
 
     }
 
 
-});
-
-router.post('/menus', function(req, res, next) {
-
-    menus.save(req.fields, req.files)
-        .then(results => {
-
-            res.send(results);
-
-        })
-        .catch(err => { res.send(err); });
 });
 
 
